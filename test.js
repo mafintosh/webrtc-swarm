b35dcc2cbf61eb1ceef7f25ee442580fdfdd2e5d
Luke Burns <lukemburns@gmail.com>
2016-09-06 18:04:11 -0400

    added three peer test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var swarm = require('./')
 var wrtc = require('electron-webrtc')()
 var crypto = require('crypto')
 
-tape('greet and close', function (t) {
+tape('two peers exchange data and close', function (t) {
   t.plan(6)
 
   var key = randomHex('64')
@@ -22,7 +22,6 @@ tape('greet and close', function (t) {
       t.equal(c.toString(), goodbye, 'goodbye received')
       peer1.close(function () {
         t.ok(1, 'peer 1 closed')
-        if (wrtc) wrtc.close()
       })
     })
   })
@@ -35,6 +34,30 @@ tape('greet and close', function (t) {
       peer2.close(function () {
         t.ok(1, 'peer 2 closed')
       })
+    })
+  })
+})
+
+tape('three peers connect and close', function (t) {
+  t.plan(10)
+
+  var key = randomHex('64')
+
+  var counts = 0
+  var peers = Array(3).fill(0).map(function () { return swarm(signalhub(key, 'https://signalhub.mafintosh.com'), {wrtc}) })
+
+  peers.forEach(function (peer, i) {
+    peer.on('peer', function () {
+      t.ok(1, ++counts + ' connections')
+      if (counts === 6) {
+        t.ok(1, 'everyone connected')
+        peers.forEach(function (peer, i) {
+          peer.close(function () {
+            t.ok(1, 'peer ' + i + ' closed')
+          })
+        })
+        if (wrtc) wrtc.close()
+      }
     })
   })
 })
